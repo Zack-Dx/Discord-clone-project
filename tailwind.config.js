df1174c6e740cce22da151ed6d70cba318267780
Abhay Bisht <abhayb2002@gmail.com>
2022-10-05 23:20:04 +0530

    Created The Support Page Using Tailwind.css

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      colors:{
+        "discord" : "#5865f2",
+        "footer":"#23272A",
+        "dis_green":"#3BA55C"
+      }
+    },
+  },
+  plugins: [],
+}
