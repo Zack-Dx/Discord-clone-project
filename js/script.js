c598f1be106051388915c81ba2678f4ab666ac11
Abdulqudus Sanusi <qudussanusi2021@gmail.com>
2022-10-06 11:49:28 -0900

    made the navbar responsive

--- js/script.js
+++ js/script.js
@@ -51,3 +51,19 @@ function switchMode() {
     document.body.style.color = "#000";
   }
 }
+
+
+let hamburger = document.querySelector('.hamburger');
+let close = document.querySelector('.close');
+let menu = document.querySelector('.menu');
+let hidden = document.querySelector('.hidden');
+
+hamburger.addEventListener('click',function(e){
+  menu.classList.add('show');
+  hidden.style.display = 'flex';
+})
+
+close.addEventListener('click',function(e){
+  menu.classList.remove('show');
+  hidden.style.display = 'none';
+})
