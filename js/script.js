21f855f853a3b90981eae41c373b82e52fb0c7a5
Shabbir Ahmad <100440185+tech0x10@users.noreply.github.com>
2022-10-07 22:24:30 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -96,3 +96,12 @@ function setDevice() {
 }
 
 setDevice();
+let mybutton = document.getElementById("btnScrollToTop");
+window.onscroll = function () { scrollFunction() };
+function scrollFunction() {
+  if (document.documentElement.scrollTop > 800) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
